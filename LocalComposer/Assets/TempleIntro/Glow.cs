00a2102050cfc4f5140ca321881c892b8597da44
Anthony Veschi <anthonyveschi@gmail.com>
2021-03-18 02:58:30 -0400

    fancy ass lighting and shit

---
+++ LocalComposer/Assets/TempleIntro/Glow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glow : MonoBehaviour
+{
+    public Material mat;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") { return; }
+        mat.SetColor("_EMISSION", new Color(1, 1, 0));
+        mat.EnableKeyword("_EMISSION");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player") { return; }
+        mat.SetColor("_EMISSION", new Color(0, 0, 0));
+        mat.DisableKeyword("_EMISSION");
+    }
+}
---
+++ LocalComposer/Assets/TempleIntro/TorchFlicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchFlicker : MonoBehaviour
+{
+    public float minTimeBetweenFlickers;
+    public float maxTimeBetweenFlickers;
+    public float minFlickerTime;
+    public float maxFlickerTime;
+    public float defaultIntensity;
+    public float flickerIntensity;
+    public Light light;
+
+    private void Start()
+    {
+        light.intensity = defaultIntensity;
+
+        StartCoroutine("FlickerCoroutine");
+    }
+
+    IEnumerator FlickerCoroutine()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(minTimeBetweenFlickers, maxTimeBetweenFlickers);
+            yield return new WaitForSeconds(waitTime);
+
+            light.intensity = flickerIntensity;
+            waitTime = Random.Range(minFlickerTime, maxFlickerTime);
+            yield return new WaitForSeconds(waitTime);
+            light.intensity = defaultIntensity;
+        }
+    }
+}
